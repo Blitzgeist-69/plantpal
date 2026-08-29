28156d1b6d57ffaa91d4cd29b4abb007d0705655
Blitzgeist-69 <ian_clifford@hotmail.com>
2026-08-29 21:09:52 +0100

    Stop tracking local db, env file and Python bytecode

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_f5tHOW6mDbLj@ep-nameless-dust-b1upbg15.c-5.eu-central-1.aws.neon.tech/canon_urban_prune_534568")
-os.environ.setdefault("SECRET_KEY", "1b!&Ap?U#}$_(xF!")
