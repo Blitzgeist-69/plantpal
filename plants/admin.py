6242f0389e6fd65d85e06016330bf63873097ded
Blitzgeist-69 <ian_clifford@hotmail.com>
2026-09-02 19:34:50 +0100

    Add layout to Django admin in plants admin.py

--- plants/admin.py
+++ plants/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Plant, CareLog
 
 # Register your models here.
-admin.site.register(Plant)
-admin.site.register(CareLog)
+
+
+class CareLogInline(admin.TabularInline):
+    model = CareLog
+    extra = 0
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = (
+        'nickname', 'species', 'user', 'location', 'water_frequency_days',
+    )
+    list_filter = ('light_needs',)
+    search_fields = ('nickname', 'species',)
+    inlines = [CareLogInline]
+
+
+@admin.register(CareLog)
+class CareLogAdmin(admin.ModelAdmin):
+    list_display = ('plant', 'action', 'date',)
+    list_filter = ('action',)
+    date_hierarchy = 'date'
